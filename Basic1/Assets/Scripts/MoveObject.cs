4dd6db3c8f77608a7d14fd4cd4da3606ac2b5a32
정영호 <gggrhrh98@naver.com>
2025-03-10 20:19:35 +0900

    dragonFlight

--- Basic1/Assets/Scripts/MoveObject.cs
+++ Basic1/Assets/Scripts/MoveObject.cs
@@ -5,17 +5,12 @@ public class MoveObject : MonoBehaviour
 
     public float speed = 5.0f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        float move = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * move * speed * Time.deltaTime);
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+        transform.position += move * speed * Time.deltaTime;
         
     }
 }
---
+++ Basic1/Assets/Scripts/VectorStudy.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class VectorStudy : MonoBehaviour
+{
+
+    public Vector2 v2 = new Vector2(10,10);
+    public Vector3 v3 = new Vector3(1, 1,1);
+    public Vector4 v4 = new Vector4(1.0f, 0.5f, 1.0f, 1.0f);    //RGB + 투명도 까지 해서 4개
+
+    void Start()
+    {
+        //Vector3 a = new Vector3(1, 0, 0);
+        //Vector3 b = new Vector3(2, 0, 0);
+
+        //Vector3 c = a + b;
+
+        //Debug.Log("Vector" + c);
+
+        //Vector3 a = new Vector3(1, 1, 0);
+        //Vector3 b = new Vector3(2, 0, 0);
+
+        //Vector3 c = a + b;
+
+        //Debug.Log("Vector" + c);
+
+        //Debug.Log("길이 : " + c.magnitude);
+
+
+        Vector3 a = new Vector3(3, 0, 0);
+
+        //정규화 normalize 
+        //벡터의 크기를 1로 만들고 방향만 유지
+        Vector3 normalizedVector = a.normalized;
+
+        Debug.Log("1의길이 방향만 설정하는 정규화 : " + normalizedVector);
+
+        //
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
