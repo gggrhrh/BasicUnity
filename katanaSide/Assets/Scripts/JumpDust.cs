223a5d10ca92b03231e7229a9104142b61669ece
정영호 <gggrhrh98@naver.com>
2025-03-26 14:47:53 +0900

    side

---
+++ katanaSide/Assets/Scripts/JumpDust.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class JumpDust : MonoBehaviour
+{
+    public float lifetime = 0.5f;
+
+    private void Awake()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+
+}
